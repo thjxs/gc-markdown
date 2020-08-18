95d4e7a14a1c20ff5c31b249190edfe2ca2bd9fb
thjxs <tanhongjianx@163.com>
2020-08-18 14:19:38 +0800

    export context

--- src/index.tsx
+++ src/index.tsx
@@ -1,4 +1,7 @@
 import Markdown from './Markdown';
+import DarkModeContext from './DarkModeContext';
 export { MarkDownProps } from './interface';
+
+export { DarkModeContext };
 
 export default Markdown;
